18cbe021c91d484309e7d5bdae4e8b9cb33de608
vladvasyuk <nazgarth@gmail.com>
2015-09-15 18:43:25 +0300

    Up/down time badge added

--- src/background.js
+++ src/background.js
@@ -2,12 +2,14 @@ var services = {
    'test-online.sbis.ru': {
       'url': 'https://test-online.sbis.ru/service/sbis-rpc-service300.dll',
       'status': false,
-      'main': true
+      'main': true,
+      'last_status_time': new Date()
    },
    'test-inside.tensor.ru': {
       'url': 'https://test-inside.tensor.ru/service/sbis-rpc-service300.dll',
       'status': false,
-      'main': false
+      'main': false,
+      'last_status_time': new Date()
    }
 };
 
@@ -24,6 +26,9 @@ var makeRequest = function(service) {
             chrome.browserAction.setIcon({
                path: 'icons/dead.png'
             });
+            if (service.status !== false) {
+               service.last_status_time = new Date();
+            }
          }
          service.status = false;
       },
@@ -33,9 +38,16 @@ var makeRequest = function(service) {
                path: 'icons/alive.png'
             });
          }
+         if (service.status !== true) {
+            service.last_status_time = new Date();
+         }
          service.status = true;
       },
       complete: function() {
+         if (service.main) {
+            chrome.browserAction.setBadgeText({
+               text: getTimeDeltaString(new Date(), service['last_status_time'])});
+         }
          setTimeout(function() {
             makeRequest(service);
          }, 5000);
@@ -43,7 +55,21 @@ var makeRequest = function(service) {
    });
 };
 
+var getTimeDeltaString = function(time1, time2) {
+   var deltaMs = time1 - time2;
+   var deltaS = Math.floor(deltaMs / 1000);
+   if (deltaS < 60) {
+      return deltaS + 's';
+   } else if (deltaS < 60*60) {
+      return Math.floor(deltaS / 60) + 'm';
+   } else {
+      return Math.floor(deltaS / 60 * 60) + 'h';
+   }
+};
+
+
 window.onload = function() {
+   chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 255, 255]});
    for (service in services) {
       if (services.hasOwnProperty(service)) {
          makeRequest(services[service]);
